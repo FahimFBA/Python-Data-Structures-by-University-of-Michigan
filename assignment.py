74e7113fd5db869531aff9ca4b384bcd233d3a59
FahimFBA <fahimbinamin@gmail.com>
2021-03-28 15:41:29 +0600

    assignment has been finished

---
+++ assignment.py
@@ -0,0 +1,13 @@
+# Use the file name mbox-short.txt as the file name
+
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+average = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    average += float(line[20:-1].strip())
+    count = count + 1
+    #print(line)
+    
+print("Average spam confidence:", (average/count))
