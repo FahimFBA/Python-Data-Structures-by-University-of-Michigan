12e5ab9fed3d66af8272c4b0c903ad9425656138
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-04-28 17:58:50 +0600

    slicing in list 1 and 2

---
+++ Manipulating List/2.py
@@ -0,0 +1,4 @@
+N = [2, 6, 86, 25, 215, 36, 4, 9, 27, 39]
+Output = N[:5] # from the beginning (index 0) and upto index 5 but not including index 5
+print(Output)
+
---
+++ Manipulating List/Sliced List.py
@@ -0,0 +1,4 @@
+N = [2, 6, 86, 25, 215, 36, 4, 9, 27, 39]
+Output = N[1:3] # from the index 1 and upto index 3 but not including index 3
+print(Output)
+
