5d27c99f509e180faee38a10e34a35b6d4bd4c8d
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-04-28 18:18:19 +0600

    list from scratch

---
+++ Manipulating List/Building A List from Scratch.py
@@ -0,0 +1,14 @@
+# We can create an empty list and then add elements using the append method
+
+var = list() # creating an empty list
+var.append('apple') # adding 'apple' to the list through the append method
+# The list stays in order and new elements are added at the end of the list by default
+var.append(120) # adding 120 to the list through the append method
+print (var) # printing the list
+
+
+
+var.append('Orange')
+var.append(180)
+print (var) # printing the list again after adding the above two new elements
+
