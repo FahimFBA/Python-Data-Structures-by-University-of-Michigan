deb0a5f2cd71f530521679d7133fae91b90a8b96
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-04-29 17:35:11 +0600

    ordered list

---
+++ Lists in Order/a.py
@@ -0,0 +1,10 @@
+# A list can hold many items and keeps those items in the order until we do something to change the order.
+
+customer = ['Alice' , 'Orca' , 'Sansa' ,'Joffry' , 'Rumel']
+customer.sort()
+print(customer)
+print(customer[2])
+
+# A list can be sorted (i.e., change its order)
+
+# The sort method (unlike in strings) means "sort yourself"
