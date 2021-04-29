ec6fcc992c898ec308b372800212fd8dd74ba6b8
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-04-29 17:17:23 +0600

    Search in list

---
+++ Searching in the List/1.py
@@ -0,0 +1,16 @@
+# Python provides two operators that let you check if an item is in a list or not
+
+
+
+
+var = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(10 in var)
+
+print(1 in var)
+
+print(22 not in var)
+
+# These are logical operators that return True or False
+
+# They do not modify the list
