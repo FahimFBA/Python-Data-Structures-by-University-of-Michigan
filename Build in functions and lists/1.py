c1d15deb9269863474dbd1c71ed84a5cbc2a05e2
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-04-30 01:55:29 +0600

    built in function

---
+++ Build in functions and lists/1.py
@@ -0,0 +1,8 @@
+# There are a number of functions built into Python that take lists as parameters
+
+nums = [3, 41, 12, 9, 74, 15]
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(sum(nums)/len(nums))
