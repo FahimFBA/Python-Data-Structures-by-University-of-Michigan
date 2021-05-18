f2f3094d44c102a3553c77a48988a52d95257cbb
FahimFBA <fahimbinamin@gmail.com>
2021-05-19 00:02:46 +0600

    comparison between lists and dictionaries in python

---
+++ Dictionary/comparing_Lists_and_Dictionaries.py
@@ -0,0 +1,20 @@
+# Dictionaries are like lists except that they use keys instead of numbers to look up values
+
+
+# Lists
+
+lst = list()
+lst.append(21)
+lst.append(183)
+print(lst)
+lst[0] = 23
+print(lst)
+
+# Dictionaries
+
+ddd = dict()
+ddd['age'] = 21
+ddd['course'] = 182
+print(ddd)
+ddd['age'] = 23
+print(ddd)
