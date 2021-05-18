a24708f6315649804be3732504c33eb69a7775b5
FahimFBA <fahimbinamin@gmail.com>
2021-05-19 00:09:18 +0600

    dictionary literals

---
+++ Dictionary/DIctionary_Literals.py
@@ -0,0 +1,11 @@
+# Dictionary Literals(Constants)
+
+# Dictionary literals use curly braces and have a list of key : value pairs
+
+# You can make an empty dictionary using empty curly braces
+
+jjj = { 'chuck' : 1 , 'fred' : 42 , 'jan' : 100 }
+print(jjj)
+
+ooo = { }
+print(ooo)
