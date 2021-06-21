6bcb283cc67bfac5f4217b26cae745277a353636
FahimFBA <fahimbinamin@gmail.com>
2021-06-21 15:54:03 +0600

    Counting Pattern

---
+++ Dictionary/main5.py
@@ -0,0 +1,11 @@
+# Counting Pattern
+
+counts = dict()
+print('Enter a line of text: ')
+line = input('')
+words = line.split()
+print('Words:' , words)
+print('Counting.....')
+for word in words:
+    counts[word] = counts(word, 0) + 1
+print('Counts', counts)
