97bb002e6f899ce4000150c95eb8d1308d1dded2
FahimFBA <fahimbinamin@gmail.com>
2021-06-25 02:14:56 +0600

    Two Iteration Variables

---
+++ Dictionary/main9.py
@@ -0,0 +1,19 @@
+# Bonus: Two Iteration Variables!
+
+# We loop through the key-value pairs in a dictionary
+# using two iteration variables
+
+# Each iteration, the first variable is the key
+# and the second variable is the corresponding value
+# for the key
+
+jjj = { 'chuck' : 1 , 'fred' : 42 , 'jan' : 100 }
+for aaa, bbb in jjj.items():
+    print(aaa, bbb)
+
+
+# Output
+
+# chuck 1
+# fred 42
+# jan 100
