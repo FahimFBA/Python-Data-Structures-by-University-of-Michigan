ae95a0433010e49fe8a11c78e9372bb75f0e65c8
FahimFBA <fahimbinamin@gmail.com>
2021-06-25 02:07:41 +0600

    Retrieving lists of keys and values

---
+++ Dictionary/main8.py
@@ -0,0 +1,12 @@
+# Retrieving lists of keys and values
+
+# You can get a list of keys, values, or items(both) from a dictionary
+
+jjj = {'chuck' : 1, 'fred' : 42, 'jan' : 100}
+print(list(jjj))
+
+print(jjj.keys())
+
+print(jjj.values())
+
+print(jjj.items())
