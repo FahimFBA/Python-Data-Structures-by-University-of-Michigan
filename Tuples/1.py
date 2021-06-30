dd569a990ec47a3d8a9d7af8270f460be010e52c
FahimFBA <fahimbinamin@gmail.com>
2021-06-30 12:25:23 +0600

    Tuples are like Lists

---
+++ Tuples/1.py
@@ -0,0 +1,16 @@
+# Tuples are like Lists
+
+# Tuples are another kind of sequence that functions much like a list
+
+# They have elements which are indexed starting at 0
+
+x = ('Glenn' , 'sally' , 'Joseph')
+print(x[2])
+
+y = (1, 9, 2)
+print(y)
+
+print(max(y))
+
+for iter in y:
+    print(iter)
