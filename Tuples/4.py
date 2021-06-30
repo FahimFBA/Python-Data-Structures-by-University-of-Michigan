1e3a56eee021b5c21186947f6012ef8d869616d5
FahimFBA <fahimbinamin@gmail.com>
2021-06-30 20:41:27 +0600

    Tuples and Assignment

---
+++ Tuples/4.py
@@ -0,0 +1,11 @@
+# Tuples and Assignment
+
+# We can also put a tuple on the left-hand side of an assignment statement
+
+# We can even omit the parentheses
+
+(x, y) = (4, 'fred')
+print(y)
+
+(a, b) = (99, 98)
+print(a)
